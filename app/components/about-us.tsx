0e43b6e1cc67f03e291cfe7474c3aaebac531d1f
nurqalik <qaliksulistyo@gmail.com>
2024-12-02 16:50:57 +0700

    Update

--- app/components/about-us.tsx
+++ app/components/about-us.tsx
@@ -1,14 +1,28 @@
 export default function AboutUs() {
-    return (
-      <section id="about" className="py-16 bg-gray-800">
-        <div className="container mx-auto px-4">
-          <h2 className="text-3xl font-bold text-center mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">About Astrophile</h2>
-          <div className="max-w-3xl mx-auto text-center">
-            <p className="text-lg text-gray-300 leading-relaxed">
-              Our club was born in 9 Sept 2024.
-            </p>
-          </div>
+  return (
+    <section id="about" className="py-16 bg-gray-800">
+      <div className="container mx-auto px-4">
+        <h2 className="text-3xl font-bold text-center mb-8 bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-pink-600">
+          About Astrophile
+        </h2>
+        <div className="max-w-3xl text-center flex flex-col gap-y-8 mx-4">
+          <p className="text-lg text-gray-300 leading-relaxed">
+            Astrophile was started on 9 September, 2024, by Rey (the founder)
+            and his team, Melin, Lita, Delphy, and Artha.
+          </p>
+          <p className="text-base text-gray-300 leading-relaxed">
+            Astrophile is a brand-new club formed in 2024. The name
+            &apos;Astrophile&apos; means someone who loves everything about the
+            sky. Because even above the sky, there&apos;s still Astrophile! The
+            founders and staff of Astrophile have a dream of creating a small
+            family within the Ayodance community. We want to be friends not just
+            in the game, but also in real life. That&apos;s why Astrophile was
+            created. We hope this club can be a solid, close-knit group where we
+            can leave the drama behind. After all, the goal of Astrophile is to
+            make new friends, not enemies.
+          </p>
         </div>
-      </section>
-    )
-  }  
+      </div>
+    </section>
+  );
+}
